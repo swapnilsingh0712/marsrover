f828391219236b89ba308ecfc050f57f012c4134
ssingh <ssingh@cnsonline.net>
2024-08-06 14:29:41 +0100

    Change the direction Strings to Constants

--- src/main/java/org/codemanship/Rover.java
+++ src/main/java/org/codemanship/Rover.java
@@ -3,6 +3,10 @@ package org.codemanship;
 import java.util.*;
 
 public class Rover {
+    public static final String NORTH = "north";
+    public static final String EAST = "east";
+    public static final String SOUTH = "south";
+    public static final String WEST = "west";
     private String direction;
 
     public Rover(String direction) {
@@ -14,11 +18,11 @@ public class Rover {
     }
 
     public void turnLeft() {
-        direction = "north";
+        direction = NORTH;
     }
 
     public void turnRight() {
-        List<String> compass = Arrays.asList("north", "east", "south", "west");
+        List<String> compass = Arrays.asList(NORTH, EAST, SOUTH, WEST);
         int currentDirectionIndex = compass.indexOf(direction);
         if (currentDirectionIndex == 3) {
             direction = compass.get(0);

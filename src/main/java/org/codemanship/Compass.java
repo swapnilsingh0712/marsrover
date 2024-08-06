19b42bf83ed21c8faad91000d10d6ebb786dea8e
ssingh <ssingh@cnsonline.net>
2024-08-06 15:13:35 +0100

    refactor list initialization

--- src/main/java/org/codemanship/Compass.java
+++ src/main/java/org/codemanship/Compass.java
@@ -8,9 +8,9 @@ public class Compass {
     public static final String EAST = "east";
     public static final String SOUTH = "south";
     public static final String WEST = "west";
+    private static final List<String> compass = Arrays.asList(NORTH, EAST, SOUTH, WEST);
 
     static String nextDirection(String currentDirection) {
-        List<String> compass = Arrays.asList(NORTH, EAST, SOUTH, WEST);
 
         int currentDirectionIndex = compass.indexOf(currentDirection);
         if (currentDirectionIndex == 3) {
